11777af873a1cd49daf0fef73a98226618ea6cbb
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-05-29 23:02:15 +0300

    add getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
-import {getFeaturedEvents} from '../dummy-data';
-import EventList from '../components/events/event-list';
-import { Fragment } from 'react';
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
+import { Fragment } from "react";
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents();
+const HomePage = (props) => {
   return (
     <Fragment>
-      <EventList items={featuredEvents}/>
+      <EventList items={props.events} />
     </Fragment>
   );
 };
 
 export default HomePage;
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 10,
+  };
+}
