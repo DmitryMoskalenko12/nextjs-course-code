3d2bc9bbc3bb1c910329bcfbc3de2e3f0328117d
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-06-28 22:14:54 +0300

    add Head component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 import { Fragment } from "react";
@@ -5,6 +6,10 @@ import { Fragment } from "react";
 const HomePage = (props) => {
   return (
     <Fragment>
+      <Head>
+        <title>NextJS Events</title>
+        <meta name='description' content='Find a lot of great events that allow you to evolve'/>
+      </Head>
       <EventList items={props.events} />
     </Fragment>
   );
